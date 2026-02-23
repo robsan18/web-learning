829cd045f5a4a56ccfe218e9ae9b0a0c8424f47a
Robsan <robsanabdi180@gmail.com>
2026-02-23 02:43:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-function completeLesson(name){
-localStorage.setItem(name,"done");
-alert("Lesson Completed!");
+let page=0;
+
+function nextPage(pages){
+if(page<pages.length-1){
+page++;
+showPage(pages);
+}
 }
 
-function checkLesson(name,id){
-if(localStorage.getItem(name)){
-document.getElementById(id).innerHTML="✅ Completed";
+function prevPage(pages){
+if(page>0){
+page--;
+showPage(pages);
+}
+}
+
+function showPage(pages){
+document.getElementById("content").innerHTML=pages[page];
+updateProgress(pages.length);
+}
+
+function updateProgress(total){
+let percent=Math.floor(((page+1)/total)*100);
+document.getElementById("bar").style.width=percent+"%";
+document.getElementById("percent").innerText=percent+"% completed";
+}
+
+/* QUIZ */
+function checkAnswer(correct){
+let result=document.getElementById("result");
+
+if(correct){
+result.innerHTML="✅ Correct!";
+result.style.color="green";
+}else{
+result.innerHTML="❌ Wrong";
+result.style.color="red";
 }
 }
